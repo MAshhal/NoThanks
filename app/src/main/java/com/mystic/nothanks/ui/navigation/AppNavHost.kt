62e41e6b4ea1a27f81338b25e839319bb0abb6b9
DrMystic (Ashhal) <techgamingyt0@gmail.com>
2024-07-07 23:12:09 +0500

    Refactored hard coded route names

--- app/src/main/java/com/mystic/nothanks/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/mystic/nothanks/ui/navigation/AppNavHost.kt
@@ -7,6 +7,7 @@ import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.mystic.nothanks.ui.screens.brands.BrandsScreen
 import com.mystic.nothanks.ui.screens.home.HomeScreen
 
 @Composable
@@ -14,7 +15,7 @@ fun AppNavHost(
     appState: AppState,
     onShowSnackbar: suspend (String, String?) -> Boolean,
     modifier: Modifier = Modifier,
-    startDestination: String = "Home"
+    startDestination: String = HOME_ROUTE
 ) {
     val navController = appState.navController
     
@@ -23,9 +24,14 @@ fun AppNavHost(
         startDestination = startDestination,
         modifier = modifier,
     ) {
-        composable("home") {
+        composable(HOME_ROUTE) {
             HomeScreen(onShowSnackbar = onShowSnackbar)
         }
+
+        composable(BRANDS_ROUTE) {
+            BrandsScreen(onShowSnackbar = onShowSnackbar)
+        }
+
     }
     
 }
@@ -34,3 +40,6 @@ fun NavHostController.safeNavigate(route: String) = navigate(route)
 
 val NavBackStackEntry.isLifecycleResumed
     get() = lifecycle.currentState == Lifecycle.State.RESUMED
+
+const val HOME_ROUTE = "home"
+const val BRANDS_ROUTE = "brands"
